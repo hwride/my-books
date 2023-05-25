4b031217f2906d54a03ff75eb2ee4d69b1f5d554
Hugo Wride <hugowride@gmail.com>
2023-05-25 10:27:27 +0100

    Add prettier.

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # My books
+
 An app for managing books you have read or would like to read.
 
 ## Getting Started
+
 1. Run the development server: `pnpm dev`
 2. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 ## Technologies
+
 1. Next.js
 2. React
 3. TypeScript
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import { Inter } from 'next/font/google'
-import Head from "next/head";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
-        <Head>
-            <title>My books</title>
-        </Head>
+      <Head>
+        <title>My books</title>
+      </Head>
       <h1>My books</h1>
     </main>
   )
