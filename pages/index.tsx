c781733cac040421fb7584d527e0f26de2c55dc7
Hugo Wride <hugowride@gmail.com>
2023-05-25 10:18:48 +0100

    Set page title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
+        <Head>
+            <title>My books</title>
+        </Head>
       <h1>My books</h1>
     </main>
   )
