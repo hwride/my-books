3364ec81238b202e093156e51642df20050bba3c
Hugo Wride <hugowride@gmail.com>
2023-05-29 15:27:25 +0100

    Use getStaticProps to get hard coded books.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-import { BookList } from '@/components/BookList'
+import { Book, BookList } from '@/components/BookList'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({books}: { books: Book[]
+}) {
   return (
     <main className={`${inter.className}`}>
       <Head>
         <title>My books</title>
       </Head>
       <h1 className="mx-auto mt-6 w-fit text-2xl">My books</h1>
-      <BookList books={[{
+      <BookList books={books} />
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props : {
+      books: [{
         title: 'The Final Empire',
         author: 'Brandon Sanderson'
       }, {
@@ -20,7 +29,7 @@ export default function Home() {
       }, {
         title: 'The Hero of Ages',
         author: 'Brandon Sanderson'
-      }]} />
-    </main>
-  )
+      }]
+    }
+  }
 }
