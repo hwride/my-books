cc631a11742da1ed1c98a78a6de869fd2dc1392a
Hugo Wride <hugowride@gmail.com>
2023-05-29 15:59:21 +0100

    Add very basic test of requesting data from MySQL DB.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { Book, BookList } from '@/components/BookList'
+import mysql from 'mysql2/promise';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,18 +19,17 @@ export default function Home({books}: { books: Book[]
 }
 
 export async function getStaticProps() {
+  let books: Book[] = [];
+  if(process.env.DATABASE_URL != null) {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL)
+    const [rows, fields] = await connection.query('SELECT title, author from Book');
+    books = rows as Book[];
+    await connection.end();
+  }
+
   return {
     props : {
-      books: [{
-        title: 'The Final Empire',
-        author: 'Brandon Sanderson'
-      }, {
-        title: 'The Well of Ascension',
-        author: 'Brandon Sanderson'
-      }, {
-        title: 'The Hero of Ages',
-        author: 'Brandon Sanderson'
-      }]
+      books
     }
   }
 }
