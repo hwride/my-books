cfd7dc1d08b37ce8f93be02ea290bd588fb1b5ce
Hugo Wride <hugowride@gmail.com>
2023-06-07 12:04:22 +0100

    Center sign up and sign in components.

--- pages/sign-in/[[...index]].tsx
+++ pages/sign-in/[[...index]].tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export default function SignInPage() {
   return (
-    <main>
+    <main className="flex h-[100dvh] items-center justify-center">
       <Head>
         <title>Sign in</title>
         <meta name="description" content="Sign in to my books" />
--- pages/sign-up/[[...index]].tsx
+++ pages/sign-up/[[...index]].tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export default function SignUpPage() {
   return (
-    <main>
+    <main className="flex h-[100dvh] items-center justify-center">
       <Head>
         <title>Sign in</title>
         <meta name="description" content="Sign up for my books" />
