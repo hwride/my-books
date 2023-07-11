8a385d67468f9eec08ae090e91086d1576e8840e
Hugo Wride <hugowride@gmail.com>
2023-07-11 14:57:31 +0100

    Change homepage to have clickable logo.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,9 +29,27 @@ export default function Home() {
           content="Manage yours books - the books you want to read, or the books you've already read."
         />
       </Head>
-      <h1 className="text-2xl">My books</h1>
-      <Link href="/readingList">Reading list</Link>
-      <Link href="/finishedBooks">Finished books</Link>
+      <MotionLink
+        href="/readingList"
+        whileHover={{ scale: 1.3 }}
+        whileFocus={{ scale: 1.3 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{
+          type: 'spring',
+          // duration: 0.5,
+          bounce: 0.5,
+        }}
+      >
+        <Image
+          src="/android-chrome-512x512.png"
+          alt="My books logo"
+          width={250}
+          height={250}
+        />
+      </MotionLink>
+      <h1 className="mt-12 text-2xl">My books</h1>
     </main>
   )
 }
+
+const MotionLink = motion(Link)
