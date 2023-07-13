cba185a0888f20b1f1ae24b5f6d6203980c72393
Hugo Wride <hugowride@gmail.com>
2023-07-13 15:30:30 +0100

    Add progressively enhanced form component.

--- pages/addBook.tsx
+++ pages/addBook.tsx
@@ -1,5 +1,4 @@
-import { Status } from '@prisma/client'
-import React, { FormEvent, useState } from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import { coreDictionary } from '@/components/dictionary/core'
 import { useRouter } from 'next/router'
@@ -7,44 +6,14 @@ import { Header } from '@/components/Header'
 import { Inter } from 'next/font/google'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Form } from '@/components/Form'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function AddBook() {
   const router = useRouter()
   const [isUpdatePending, setIsUpdatePending] = useState(false)
-
-  const nameTitle = 'title'
-  const nameAuthor = 'author'
-  async function addBook(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-
-    setIsUpdatePending(true)
-
-    const form = e.currentTarget
-    const data = new FormData(form)
-    const title = data.get(nameTitle) as Status
-    const author = data.get(nameAuthor) as Status
-    const options = {
-      method: form.method,
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify({
-        title,
-        author,
-      }),
-    }
-
-    const r = await fetch(form.action, options)
-
-    if (r.ok) {
-      const newBook = await r.json()
-      router.push(`/book/${newBook.id}`)
-    } else {
-      console.error(`Error when creating book`)
-    }
-
-    setIsUpdatePending(false)
-  }
+  const [isSuccess, setIsSuccess] = useState(false)
 
   return (
     <main className={`${inter.className} mx-auto max-w-screen-md`}>
@@ -53,11 +22,17 @@ export default function AddBook() {
         <meta name="robots" content="noindex" />
       </Head>
       <Header heading="Add a book" />
-      <form
+      <Form
         action="/api/book"
         method="post"
         className="mx-auto max-w-md p-4"
-        onSubmit={(e) => addBook(e)}
+        isUpdatePending={isUpdatePending}
+        setIsUpdatePending={setIsUpdatePending}
+        onSuccess={(newBook) => {
+          setIsSuccess(true)
+          router.push(`/book/${newBook.id}`)
+        }}
+        onError={() => console.error(`Failed to add book`)}
       >
         <div className="mb-4 grid grid-cols-[auto_1fr] grid-rows-2 items-center gap-x-2 gap-y-2">
           <label htmlFor="new-book-title" className="row-start-1 block">
@@ -82,10 +57,15 @@ export default function AddBook() {
             className="col-start-2 row-start-2 self-stretch"
           />
         </div>
-        <Button className="mx-auto block" disabled={isUpdatePending}>
+        {/* After a successful creation isUpdatePending is briefly false before routing completes. So also include
+           check which disables it in case of success to wait for routing to finish. */}
+        <Button
+          className="mx-auto block"
+          disabled={isUpdatePending || isSuccess}
+        >
           Add book
         </Button>
-      </form>
+      </Form>
     </main>
   )
 }
