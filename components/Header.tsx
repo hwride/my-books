9e2ed91bd5c326b9c3d7b1d42ba345f0385e600a
Hugo Wride <hugowride@gmail.com>
2023-07-14 14:10:34 +0100

    Fix dynamic class being purged by Tailwind.
    Previously calculated class with variables, but it appears that Tailwind
    purge can't detect it then (makes sense), so it purges it and it does
    not work at runtime. Instead hard-code the values.

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,12 @@ import { MenuBar } from '@/components/MenuBar'
 import { UserButton } from '@clerk/nextjs'
 import { ReactNode } from 'react'
 
-const userButtonWidthRem = 2
-const userButtonMarginRem = 1
-const userButtonSize = userButtonWidthRem + userButtonMarginRem
-
 export function Header({ heading }: { heading: ReactNode }) {
   heading = 'The Curious Incident of the Dog in the Night-Time'
   return (
     <div>
-      <h1
-        className={`mb-2 ml-auto mr-[${userButtonSize}rem] mt-4 w-fit px-page text-2xl`}
-      >
+      {/* mr-[3rem] is to ensure we make room for the absolutely positioned user icon. */}
+      <h1 className={`mb-2 ml-auto mr-[3rem] mt-4 w-fit px-page text-2xl`}>
         {/* If no heading include a non breaking space to ensure we still reserve the space
             If changing this make sure you check the case when coming from home page as heading in empty initially */}
         {heading ? heading : <>&nbsp;</>}
