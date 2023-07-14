2c9df8f7cba3e57b2ff2666c588cde87515bb400
Hugo Wride <hugowride@gmail.com>
2023-07-14 10:10:37 +0100

    Move nav to shared layout.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import { Header } from '@/components/Header'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${inter.className} mx-auto h-[100dvh] max-w-screen-md`}>
       <ClerkProvider {...pageProps}>
+        <Header heading={''} />
         <Component {...pageProps} />
         <Analytics />
       </ClerkProvider>
-    </>
+    </main>
   )
 }
