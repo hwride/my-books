d07e67b7f9f9c6213ed211286ebd4d2f9dea4bc9
Hugo Wride <hugowride@gmail.com>
2025-04-26 15:54:49 +0100

    Add button to clear input
    #68

--- components/ui/input-file.tsx
+++ components/ui/input-file.tsx
@@ -5,22 +5,53 @@ import { cn } from '@/lib/utils'
 export interface InputFileProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const InputFile = React.forwardRef<HTMLInputElement, InputFileProps>(
-  ({ className, ...props }, ref) => {
-    return (
+function InputFile({ className, onChange, ...props }: InputFileProps) {
+  const ref = React.useRef<HTMLInputElement>(null)
+  const [hasFile, setHasFile] = React.useState(false)
+
+  const clearInput = () => {
+    if (ref.current) {
+      ref.current.value = ''
+      setHasFile(false)
+    }
+  }
+
+  const onFileChange: React.ChangeEventHandler<HTMLInputElement> = (evt) => {
+    setHasFile(
+      Boolean(ref.current && ref.current.files && ref.current.files.length > 0)
+    )
+    if (onChange) onChange(evt)
+  }
+
+  return (
+    <div className="flex items-center gap-2">
       <input
         type="file"
         className={cn(
-          'px-1 py-1 text-sm font-medium',
-          'file:rounded-md file:bg-slate-900 file:text-sm file:font-medium file:text-slate-50 file:transition-colors file:hover:bg-slate-900/90 file:focus-visible:outline-none file:focus-visible:ring-1 file:focus-visible:ring-slate-400 file:dark:bg-slate-50 file:dark:text-slate-900 file:dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-800',
+          'w-full py-1 text-sm font-medium ',
+          'file:me-4 file:rounded-md file:bg-slate-900 file:px-3 file:py-1 file:text-sm file:font-medium file:text-slate-50 file:transition-colors file:hover:bg-slate-900/90 file:focus-visible:outline-none file:focus-visible:ring-1 file:focus-visible:ring-slate-400 file:dark:bg-slate-50 file:dark:text-slate-900 file:dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-800',
           className
         )}
         ref={ref}
+        onChange={onFileChange}
         {...props}
       />
-    )
-  }
-)
+      {hasFile && (
+        <button
+          type="button"
+          onClick={clearInput}
+          disabled={!hasFile}
+          className={cn(
+            'rounded-md bg-red-500 px-2 py-1 text-xs font-semibold text-white hover:bg-red-600'
+          )}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  )
+}
+
 InputFile.displayName = 'InputFile'
 
 export { InputFile }
